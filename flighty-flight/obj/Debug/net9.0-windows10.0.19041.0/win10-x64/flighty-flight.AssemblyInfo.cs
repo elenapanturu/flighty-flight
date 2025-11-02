4d82907c666203adf05c6b54810e38f095712444
Elena Panturu <elenapanturu29@gmail.com>
2025-11-02 16:30:53 +0200

    Removed unnecessary info

--- flighty-flight/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/flighty-flight.AssemblyInfo.cs
+++ flighty-flight/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/flighty-flight.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flighty-flight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+17cd16f37e1954e767200ca382058f0b7ae89c82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+07d4232d34bd0606bfe43839dfb636e5c850c572")]
 [assembly: System.Reflection.AssemblyProductAttribute("flighty-flight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flighty-flight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
